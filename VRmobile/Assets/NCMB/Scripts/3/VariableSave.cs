f6c26d5cf3f29acbef500cdff3704a76780a81d1
DA\nwuser <goroumakisimamu810@gmail.com>
2019-01-28 10:29:39 +0900

    途中のやつ

--- VRmobile/Assets/NCMB/Scripts/3/VariableSave.cs
+++ VRmobile/Assets/NCMB/Scripts/3/VariableSave.cs
@@ -7,78 +7,78 @@ public class VariableSave : MonoBehaviour {
     /// <summary>
     /// 0:未入力 1:7～9才 2:10～12才 3:13～19才 4:20代 5:30代 6:40代 7:50代 8:60代以上
     /// </summary>
-    public int age = 0;
+    public int db_age = 0;
     /// <summary>
     /// 0:未入力 1:男性 2:女性
     /// </summary>
-    public int sex = 0;
+    public int db_sex = 0;
     /// <summary>
     /// クリアタイム
     /// </summary>
-    public float time = 0f;
+    public float db_time = 0f;
     /// <summary>
     /// 死んだ階層
     /// </summary>
-    public int floor = 0;
+    public int db_floor = 0;
     /// <summary>
     /// 死んだX座標
     /// </summary>
-    public float deadPointX = 0f;
+    public float db_deadPointX = 0f;
     /// <summary>
     /// 死んだZ座標
     /// </summary>
-    public float deadPointZ = 0f;
+    public float db_deadPointZ = 0f;
     /// <summary>
     /// 火の死亡回数
     /// </summary>
-    public  static int deadFire = 0;
+    public  static int db_deadFire = 0;
     /// <summary>
     /// 煙の死亡回数
     /// </summary>
-    public static int deadSmoke = 0;
+    public static int db_deadSmoke = 0;
     /// <summary>
     /// ドラフトの死亡回数
     /// </summary>
-    public static int deadDraft = 0;
+    public static int db_deadDraft = 0;
     /// <summary>
     /// クリアしたか
     /// </summary>
-    public bool clear = false;
+    public bool db_clear = false;
     /// <summary>
     /// アンケート01 VRを他にプレイしたことがあるか。
     /// 0:未入力 1:はい 2:いいえ
     /// </summary>
-    public int quesVR = 0;
+    public int db_quesVR = 0;
     /// <summary>
     /// アンケート02 ゲームをプレイして気持ち悪くなったか。 
     /// 0:未入力 1:はい 2:いいえ
     /// </summary>
-    public int quesDrunk = 0;
+    public int db_quesDrunk = 0;
     /// <summary>
     /// アンケート03 ゲームは全体を通して分かりやすかったか。
     /// 0:未入力 1:はい 2:いいえ
     /// </summary>
-    public int quesMove = 0;
+    public int db_quesMove = 0;
     /// <summary>
     /// アンケート03_1 何が分かりずらかったか。(3の質問でいいえを答えた人用)
     /// 0:未入力 1:移動操作 2:消火器の使い方 3:しゃがみの仕方 4:1+2 5:2+3 6:1+3 7:全部
     /// </summary>
-    public int quesMoveNot = 0;
+    public int db_quesMoveNot = 0;
 
     public bool SOUSIN = false;
 
 
     // Use this for initialization
     void Start () {
-        floor = MainDataManager.getFloor();
-        deadFire = MainDataManager.getFireDead();
-        deadSmoke = MainDataManager.getSmokeDead();
-        deadDraft = MainDataManager.getDraftDead();
+        db_floor = MainDataManager.getFloor();
+        db_deadFire = MainDataManager.getFireDead();
+        db_deadSmoke = MainDataManager.getSmokeDead();
+        db_deadDraft = MainDataManager.getDraftDead();
 	}
 
     void Update() {
-        Debug.Log("火" + deadFire);
-        Debug.Log("煙" + deadSmoke);
+        Debug.Log("火" + db_deadFire);
+        Debug.Log("煙" + db_deadSmoke);
         if (Input.GetKeyDown(KeyCode.S)) {
             SOUSIN = true;
         }
@@ -90,24 +90,24 @@ public class VariableSave : MonoBehaviour {
     }
 
 
-    public void SaveVariableData(int age, int sex, float time, int floor, float deadPointX, float deadPointZ, int deadFire, int deadSmoke, int deadDraft, bool clear, int QuesVR, int QuesDrunk, int QuesMove, int QuesMoveNot) {
+    public void SaveVariableData(int age , int sex , float time , int floor , float deadPointX, float deadPointZ, int deadFire, int deadSmoke, int deadDraft, bool clear, int QuesVR, int QuesDrunk, int QuesMove, int QuesMoveNot) {
 
         NCMBObject obj = new NCMBObject("VariableData");
 
-        obj.Add("Age", age);
-        obj.Add("Sex", sex);
-        obj.Add("Time", time);
-        obj.Add("Floor", floor);
-        obj.Add("DeadPointX", deadPointX);
-        obj.Add("DeadPointZ" , deadPointZ);
-        obj.Add("DeadFire" , deadFire);
-        obj.Add("DeadSmoke" , deadSmoke);
-        obj.Add("DeadDraft" , deadDraft);
-        obj.Add("Clear", clear);
-        obj.Add("QuesVR" , quesVR);
-        obj.Add("QuesDrunk" , quesDrunk);
-        obj.Add("QuesMove" , quesMove);
-        obj.Add("QuesMoveNot" , quesMoveNot);
+        obj.Add("Age",          db_age);
+        obj.Add("Sex",          db_sex);
+        obj.Add("Time",         db_time);
+        obj.Add("Floor",        db_floor);
+        obj.Add("DeadPointX",   db_deadPointX);
+        obj.Add("DeadPointZ" ,  db_deadPointZ);
+        obj.Add("DeadFire" ,    db_deadFire);
+        obj.Add("DeadSmoke" ,   db_deadSmoke);
+        obj.Add("DeadDraft" ,   db_deadDraft);
+        obj.Add("Clear",        db_clear);
+        obj.Add("QuesVR" ,      db_quesVR);
+        obj.Add("QuesDrunk" ,   db_quesDrunk);
+        obj.Add("QuesMove" ,    db_quesMove);
+        obj.Add("QuesMoveNot" , db_quesMoveNot);
 
         obj.SaveAsync((NCMBException e) => {
 
@@ -123,7 +123,7 @@ public class VariableSave : MonoBehaviour {
     }
 
     public void Save() {
-        SaveVariableData(age , sex , time , floor , deadPointX , deadPointZ , deadFire , deadSmoke , deadDraft , clear, quesVR, quesDrunk, quesMove, quesMoveNot);
+        SaveVariableData(db_age , db_sex , db_time , db_floor , db_deadPointX , db_deadPointZ , db_deadFire , db_deadSmoke , db_deadDraft , db_clear , db_quesVR , db_quesDrunk , db_quesMove , db_quesMoveNot);
         Debug.Log("セーブしたよ");
     }
 
@@ -131,16 +131,16 @@ public class VariableSave : MonoBehaviour {
     //データに関する各項目の取得するための関数
     public static int getFireDead()
     {
-        return deadFire;
+        return db_deadFire;
     }
 
     public static int getSmokeDead()
     {
-        return deadSmoke;
+        return db_deadSmoke;
     }
 
     public static int getDraftDead()
     {
-        return deadDraft;
+        return db_deadDraft;
     }
 }
